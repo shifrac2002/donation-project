f7457b0c4a057104f2274519d01c9014e1190dd7
Leah Cohen <shifrac2002@gmail.com>
2025-08-14 20:42:56 +0300

    change routs

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -15,31 +15,28 @@ import GitHubSuccess from '../src/components/GitHubSuccess'
 
 
 function App() {
-  {
     return (
       <Router>
         <Routes>
           <Route path="/" element={<Navigate replace to="/Donates" />} />
-          <Route path="/SignUp" exact element={<SignUp />} />
-          <Route path="/LogIn" exact element={<LogIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/LogIn" element={<LogIn />} />
           <Route path="/github-success" element={<GitHubSuccess />} />
-          <Route path="/Donates/Shul/ItemsOfShul/LogIn" exact element={<LogIn />} />
-          <Route path="/Gabay/*" exact element={<Gabay />} />
-          <Route path="/Donates/*" exact element={<Donates />} />
-          {/* <Route path="/Donates/Exit" exact element={<Donates />} /> */}
-          <Route path="/Donates/Shul/ItemsOfShul/Confirmation" exact element={<Confirmation />} />
-          <Route path="/Donates/DonationByItem/ShulsOfItem/LogIn" exact element={<LogIn />} />
-          <Route path="/Donates/DonationByItem/ShulsOfItem/Confirmation" exact element={<Confirmation />} />
-          <Route path="/Donates/item/shul/Confirmation" exact element={<Confirmation />} />
-          <Route path="/Donates/DonationByItem/ShulsOfItem" exact element={<ShulsOfItem />} />
-          <Route path="/Donates/DonationsDonated" exact element={<DonationsDonated />} />
-          <Route path="/Donates/Shul/ItemsOfShul" exact element={<ItemsOfShul />} />
-          <Route path="/Donates/FreeDonation" exact element={<FreeDonation />} />
-          <Route path="/Donates/DonationByItem" exact element={<DonationByItem />} />
+          <Route path="/Donates/Shul/ItemsOfShul/LogIn" element={<LogIn />} />
+          <Route path="/Gabay/*" element={<Gabay />} />
+          <Route path="/Donates/*" element={<Donates />} />
+          <Route path="/Donates/Shul/ItemsOfShul/Confirmation" element={<Confirmation />} />
+          <Route path="/Donates/DonationByItem/ShulsOfItem/LogIn" element={<LogIn />} />
+          <Route path="/Donates/DonationByItem/ShulsOfItem/Confirmation" element={<Confirmation />} />
+          <Route path="/Donates/item/shul/Confirmation" element={<Confirmation />} />
+          <Route path="/Donates/DonationByItem/ShulsOfItem" element={<ShulsOfItem />} />
+          <Route path="/Donates/DonationsDonated" element={<DonationsDonated />} />
+          <Route path="/Donates/Shul/ItemsOfShul" element={<ItemsOfShul />} />
+          <Route path="/Donates/FreeDonation" element={<FreeDonation />} />
+          <Route path="/Donates/DonationByItem" element={<DonationByItem />} />
           <Route path="/Donates/UpdateUser" element={<UpdateUser />} />
         </Routes>
       </Router>
     );
-  }
 }
 export default App;
